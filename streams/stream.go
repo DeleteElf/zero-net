449dc90c740c3c7325b558d3b390fc8ff68a6d0a
chenhao <48207475@qq.com>
2026-05-14 17:38:03 +0800

    1, 增加流控

---
+++ streams/stream.go
@@ -0,0 +1,172 @@
+package streams
+
+import (
+	"context"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/DeleteElf/network-quic/utils"
+	"github.com/quic-go/quic-go"
+)
+
+// signSalt, 使用 openssl rand -hex 32 生成，每个版本不一样，定期删除过旧的版本
+const (
+	STREAM_SIGN_VER_NEWEST  = "1.0"
+	STREAM_SIGN_SALT_NEWEST = "fcfd186998e061eb3e297be7da42fe017fc8269f2963e2977502c676bae1068c"
+
+	STREAM_NETWORK_TCP  = "tcp"
+	STREAM_NETWORK_UDP  = "udp"
+	STREAM_NETWORK_TUN  = "tun"
+	STREAM_NETWORK_SOCK = "sock"
+)
+
+type StreamInfo struct {
+	Id      string `json:"id"`
+	Version string `json:"v"` // 客户端的版本号
+
+	Ts   int64  `json:"t"` // 用法见 ValidateStreamInfo
+	Sign string `json:"s"` // 客户端的签名，用于服务端校验客户端的合法性
+
+	Index int `json:"i"`
+}
+
+var ss = map[string]string{
+	STREAM_SIGN_VER_NEWEST: STREAM_SIGN_SALT_NEWEST,
+}
+
+func ValidateStreamInfo(info *StreamInfo) error {
+	diffTs := time.Now().Unix() - info.Ts
+	if diffTs > 300 || diffTs < -300 {
+		return fmt.Errorf("invalid ts")
+	}
+	salt, ok := ss[info.Version]
+	if !ok {
+		return fmt.Errorf("invalid ver")
+	}
+	sign := utils.EncryptBytes([]byte(fmt.Sprintf("%s_%d", salt, info.Ts)))
+	if sign != info.Sign {
+		return fmt.Errorf("invalid sign")
+	}
+	return nil
+}
+
+func CloseStream(stream *quic.Stream) error {
+	stream.CancelRead(0)
+	err := stream.Close()
+	stream.CancelWrite(0)
+	return err
+}
+
+func CreateStream(quicConn *quic.Conn, info StreamInfo) (*quic.Stream, error) {
+	info.Version = STREAM_SIGN_VER_NEWEST
+	info.Sign = utils.EncryptBytes([]byte(fmt.Sprintf("%s_%d", STREAM_SIGN_SALT_NEWEST, info.Ts)))
+	jsonInfo, err := json.Marshal(info)
+	if err != nil {
+		return nil, err
+	}
+	stream, err := quicConn.OpenStreamSync(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+	if err = SendStreamData(stream, jsonInfo); err != nil {
+		err = stream.Close()
+		if err != nil {
+			return nil, err
+		}
+		return nil, err
+	}
+	return stream, nil
+}
+
+func ReadStreamInfo(stream *quic.Stream) (*StreamInfo, error) {
+	streamId := stream.StreamID()
+	infoLen, err := ReadStreamLen(stream)
+	if err != nil {
+		return nil, err
+	}
+	if infoLen < 0 || infoLen > 1024 {
+		slog.Warn("quic stream recv len invalid", slog.Any("streamId", streamId), slog.Int64("infoLen", infoLen))
+		return nil, fmt.Errorf("invalid stream info")
+	}
+	jsonInfo := make([]byte, infoLen)
+	if err := ReadStream(stream, jsonInfo); err != nil {
+		return nil, err
+	}
+	var info StreamInfo
+	err = json.Unmarshal(jsonInfo, &info)
+	return &info, err
+}
+
+func ReadStreamData(stream *quic.Stream, data []byte) ([]byte, error) {
+	dataLen, err := ReadStreamLen(stream)
+	if err != nil {
+		return nil, err
+	}
+	if dataLen < 0 || (data == nil && dataLen > 10*1024*1024) || (data != nil && dataLen > int64(len(data))) {
+		streamId := stream.StreamID()
+		slog.Warn("quic stream recv len invalid", slog.Any("streamId", streamId), slog.Int64("dataLen", dataLen))
+		return nil, fmt.Errorf("invalid stream data")
+	}
+	if dataLen == 0 {
+		return nil, nil
+	}
+	if data == nil {
+		data = make([]byte, dataLen)
+	} else {
+		data = data[:dataLen]
+	}
+	if err := ReadStream(stream, data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func SendStreamData(stream *quic.Stream, buf []byte) error {
+	infoLen := int64(len(buf))
+	var head [binary.MaxVarintLen64]byte
+	headLen := binary.PutVarint(head[:], infoLen)
+
+	if _, err := stream.Write(head[:headLen]); err != nil {
+		return err
+	}
+	if infoLen > 0 {
+		if _, err := stream.Write(buf); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func ReadStreamLen(stream *quic.Stream) (int64, error) {
+	headLen := 0
+	var head [binary.MaxVarintLen64]byte
+	for headLen < binary.MaxVarintLen64 {
+		if _, err := stream.Read(head[headLen : headLen+1]); err != nil {
+			return 0, err
+		}
+		if head[headLen] < 0x80 {
+			headLen++
+			break
+		}
+		headLen++
+	}
+	dataLen, _ := binary.Varint(head[:headLen])
+	return dataLen, nil
+}
+
+func ReadStream(stream *quic.Stream, buf []byte) error {
+	curLen := 0
+	for {
+		readLen, err := stream.Read(buf[curLen:])
+		if err != nil {
+			return err
+		}
+		curLen += readLen
+		if curLen == len(buf) {
+			return nil
+		}
+	}
+}
