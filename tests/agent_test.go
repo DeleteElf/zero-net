1abdfd509fcd30cb503ce4a2e128b0c3b19c7ca9
chenhao <48207475@qq.com>
2026-09-14 17:40:41 +0800

    完成单元测试

--- tests/agent_test.go
+++ tests/agent_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/DeleteElf/zero-net/client"
 	"github.com/DeleteElf/zero-net/framework/network"
 	"github.com/DeleteElf/zero-net/framework/utils"
+	"io"
 	"log/slog"
 	"strconv"
 	"testing"
@@ -27,7 +28,7 @@ func angetMessageHandler(sock *network.Socket, channelIndex int) {
 		if sock.IsClosed {
 			break
 		}
-		_, err := sock.ReceiveDataToBuffer(channelIndex) //这个会卡住等待
+		_, err := sock.ReceiveDataToStreamReader(channelIndex) //这个会卡住等待
 		if err != nil {
 			slog.Error(err.Error())
 			break
@@ -35,12 +36,13 @@ func angetMessageHandler(sock *network.Socket, channelIndex int) {
 		if sock.IsClosed {
 			break
 		}
-		currentBuffer := sock.StreamChannels[channelIndex].Buffer
+		currentBuffer := sock.StreamChannels[channelIndex].BufferStream
 		if currentBuffer == nil {
 			break
 		}
-		sock.StreamChannels[channelIndex].Buffer = nil
-		msg := string(currentBuffer.Data)
+		sock.StreamChannels[channelIndex].BufferStream = nil
+		data, _ := io.ReadAll(currentBuffer.Reader)
+		msg := string(data)
 		slog.Debug("收到数据：", slog.Int("channelId", currentBuffer.ChannelId), slog.String("msg", msg),
 			slog.String("clientId", currentBuffer.ClientId))
 		if msg == "bye" {
